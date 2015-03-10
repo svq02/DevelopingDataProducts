1161a659f5a5a5bed57f7ea86ba3273c9226bd08
Seth Veitzer <seth.veitzer@gmail.com>
2015-03-10 17:54:20 -0600

    server, ui, and data file for this webapp

---
+++ server.R
@@ -0,0 +1,85 @@
+################################################################################
+##
+## @file        server.R
+##
+## @brief       Server functions for reactive app
+##
+##
+## Copyright &copy; 2015 Seth A. Veitzer
+## Please do not plagarize
+##
+################################################################################
+      
+#required libraries
+library(shiny)
+
+#load all data
+stopping <- read.csv('stopping.csv', sep=' ')
+nRows <- dim(stopping)[1]
+
+shinyServer(
+  function(input, output, session) {
+    col <- rainbow(6)
+    legend <- NULL
+    legendCols <- NULL
+# does not include mean checkbox
+    checkBoxes = c('checkBoxHydrogen', 'checkBoxAdipose', 'checkBoxBone', 'checkBoxLead', 'checkBoxMuscle', 'checkBoxWater')
+    materials = c('Hydrogen', 'Adipose Tissue', 'Compact Bone', 'Lead', 'Striated Muscle', 'Liquid Water')
+
+# Render a plot of stopping power when check boxes are selected
+    output$plot <- renderPlot({ 
+# Reset the sums for calculating the mean
+      sum <- rep(0.0, nRows)
+      nBoxesChecked <- 0
+      if (input$logAxes) {
+        log <- 'xy'; xlim <- c(1e-3,1e4); ylim <- c(1,1e4) 
+      } else {
+        log <- ''; xlim <- c(0,2); ylim <- c(0,4000)
+      }
+      plot(stopping[,1],stopping[,1], type='n', xlim=xlim, ylim=ylim, log=log, 
+           xlab='Proton Kinetic Energy (MeV)', 
+           ylab='Stopping Power (Mev cm^2/g)', main='Total Stopping Power')
+      for (i in 1:length(checkBoxes)) {
+# Only plot if the checkbox is checked
+        if (input[[checkBoxes[i]]]) {
+          lines(stopping[,1],stopping[,i+1], lwd=3, col=col[i])
+          legend <- c(legend, materials[i]); legendCols <- c(legendCols, col[i])
+          sum <- sum + stopping[,i+1]
+          nBoxesChecked <- nBoxesChecked + 1
+        }
+      }
+# plot the mean if the box is checked and not the only checked box
+      if (input[['checkBoxMean']]) {
+        if (sum(sum) != 0.0) {
+          lines(stopping[,1],sum/nBoxesChecked, lwd=3, col='black')
+        legend <- c(legend, 'Mean Stopping Power'); legendCols <- c(legendCols, 'black')
+        }
+      }
+
+# print the legend depending on what is being displayed and log axes or not
+      if (length(legend) != 0) {
+        if (input$logAxes) {
+          legend(1e3, 3000, legend, lty=1, lwd=3, col=legendCols)
+        } else {
+          legend(1, 3000, legend, lty=1, lwd=3, col=legendCols)
+        }
+      }
+    })
+
+# reset check boxes if the button is pushed
+    observe({
+      if (input$resetPlot > 0) {
+        for (i in 1:length(checkBoxes)) {
+          updateCheckboxInput(session=session, checkBoxes[i], value=FALSE)
+        }
+        updateCheckboxInput(session=session, inputId="checkBoxMean", value=FALSE)
+        updateCheckboxInput(session=session, inputId="checkBoxHydrogen", value=TRUE)
+        updateCheckboxInput(session=session, inputId="logAxes", value=TRUE)
+      }
+    })
+
+  }
+)
+
+
+
---
+++ ui.R
@@ -0,0 +1,71 @@
+################################################################################
+##
+## @file        ui.R
+##
+## @brief       Widgets and layout for sample shiny web app
+##
+##
+## Copyright &copy; 2015 Seth A. Veitzer
+## Please do not plagarize
+##
+################################################################################
+
+shinyUI(pageWithSidebar(
+  headerPanel('Proton Stopping Power'),
+  sidebarPanel(
+    checkboxInput("checkBoxHydrogen", label = "Hydrogen", value = TRUE),
+    checkboxInput("checkBoxAdipose", label = "Adipose Tissue", value = FALSE),
+    checkboxInput("checkBoxBone", label = "Compact Bone", value = FALSE),
+    checkboxInput("checkBoxLead", label = "Lead", value = FALSE),
+    checkboxInput("checkBoxMuscle", label = "Striated Muscle", value = FALSE),
+    checkboxInput("checkBoxWater", label = "Liquid Water", value = FALSE),
+    checkboxInput("checkBoxMean", label = "Mean Stopping Power", value = FALSE),
+    br(), hr(),
+    checkboxInput("logAxes", label = "Show log axes", value = TRUE),
+    hr(),
+    actionButton('resetPlot', 'Reset Plot')
+  ),
+  mainPanel(
+h1('Introduction'),
+p('Proton Stopping Power is a measure of how fast a proton loses energy when
+traveling in a particular material. When a charged particle like a proton moves
+through a solid, gas, or liquid material, it loses energy through collisions
+with the material nuclei and electrons in the material. The rate at which energy
+is lost depends on the energy of the charged particle, and the properties of the
+particular material. The rate at which energy is lost to the material is called
+the Stopping Power'),
+
+p('A plot of the stopping power vs. particle energy always has a peak, at which
+most of the energy is lost, called the Bragg Peak. Typically, nealy all of a
+particle\'s energy is deposited in a small physical region due to the Bragg peak.
+This is especially useful in medical applications such as proton cancer
+therapies, where a beam of protons can be tuned so that most of the energy of
+the beam is delivered to a specific point, say a tumor, while very little energy
+is deposited outside of that area. This technique allows for proton therapies to
+treat tumors in sensitive areas such as eyes where traditional radiation
+therapies can not be used.'),
+
+h1('WebApp Documentation'),
+strong("Don\'t worry about the physics of stopping power!"),
+('The purpose of this web app
+is to demonstrate how to create a reactive shiny web app using R, not to produce
+new science.  To use this app, select one or more target materials from the
+check boxes on the side panel. This will plot the proton stopping power over a
+range of energies for that material.  You can look at the stopping powers on a
+log-log scale by checking the appropriate check box on the side panel. Further,
+checking the "Show Mean" check box will compute the average stopping power of
+all the materials that are selected at the time. Clicking the "Reset Plots"
+button will reset the plot to its original state. In order to see the Bragg peak
+in more detail, unselect the "Log Axes" checkbox.'),
+
+    plotOutput('plot'),
+
+h1('Data Source Reference'),
+('The stopping power data used here was scraped from the'),
+a(href="http://www.nist.gov/pml/data/star/index.cfm", target="_blank", "NIST website"),
+('that tabulates stopping powers for protons, electrons, and helium atoms (alpha particles).
+You can find additional information on the physics of stopping powers, applications in
+materials and accelerator science, as well as information about the models and experiments
+that go into building the data tables at that website.')
+  )
+))
